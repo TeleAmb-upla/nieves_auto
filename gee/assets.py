fbd6794bd8bf5018bfe1cd658e79cab591e64754
Erick G <silversky54@users.noreply.github.com>
2023-03-15 19:25:58 -0300

    General Improvements to GEE Asset functions

--- gee/assets.py
+++ gee/assets.py
@@ -7,44 +7,55 @@ from utils import logs
 # Constants. Used as Defaults in case no alternative is provided.
 GEE_PATHPREFIX = 'projects/earthengine-legacy/assets/'
 
-def get_asset_list(parent:str, asset_type=None):
+def get_asset_list(parent:str, asset_type=None, recursive:bool=False):
     '''
-    Gets list assets from an assets folder in GGE 
+    lists assets from an assets folder or Image Collection in GEE. User can specify what type of assets to list. 
 
     Args: 
         parent: path to the parent folder of the assets
         asset_type: indicats asset types to list.  
+        recursive: Recursively search for assets in sub-folders
 
     reference: https://github.com/spatialthoughts/projects/blob/master/ee-python/list_all_assets.py
     '''
+    # TODO: need to verify that asset_type comes as a single string or list
+    if type(asset_type) == str:
+        asset_type=[asset_type]
+
+    # Get information of path to list 
     try:
         parent_asset = ee.data.getAsset(parent)
-        # If not a folder raise an error
-        if parent_asset['type'] not in ['FOLDER','IMAGE_COLLECTION']:
-            raise
-        
-    except:
-        logs.print_log(f"Can't find assets folder: {parent}", 'ERROR')
-        sys.exit(0)
-        #raise
-    parent_id = parent_asset['name']
-    parent_type = parent_asset['type']
-    asset_list = []
-    try:
-        child_assets = ee.data.listAssets({'parent': parent_id})['assets']
-    except:    
-        logs.print_log(f"Can't list objects in: {parent}", 'WARNING')
+        parent_id = parent_asset['name']
+        parent_type = parent_asset['type']
+        # If path is not a folder or image collection raise an error
+        if parent_type not in ['FOLDER','IMAGE_COLLECTION']:
+            raise ValueError('Path provided is not a Folder or Image Collection')
+    except ValueError as e:
         raise
+    except Exception as e:
+        logging.error(f"Can't find assets folder")
+        raise
+    
+    try:
+        asset_list = []
+        child_assets = ee.data.listAssets({'parent': parent_id})['assets']
+        if type(child_assets) != list:
+            raise Exception(f'Expecting a \'list\' of assets but got \'{type(asset_list)}\' instead')
+    except Exception as e:
+        logging.warning(f"Can't list objects in: {parent}")
+        logging.warning(e)
+        raise
+
+    # Get assets from list 
     for child_asset in child_assets:
         child_id = child_asset['name']
         child_type = child_asset['type']
         # print(f"{child_id}:{child_type}")
         if child_type in ['FOLDER','IMAGE_COLLECTION']:
             # Recursively call the function to get child assets
-            asset_list.extend(get_asset_list(child_id))
+            if recursive:
+                asset_list.extend(get_asset_list(child_id))
         else:
-            if type(asset_type) == str:
-                asset_type=[asset_type]
             if child_type in asset_type or child_type == None:
                 asset_list.append(child_id)
     return asset_list
@@ -62,22 +73,22 @@ def check_asset_exists(asset: str, asset_type=None):
     '''
 
     asset_path = pathlib.Path(asset).parent.as_posix()
-    logging.debug(f"Searching for: {asset}")
-
-    # Get list of assets in given path
     try: 
+        asset_found = False
+        # Get list of assets in given path
         asset_list = get_asset_list(asset_path, asset_type=asset_type)
-        logging.debug('Found {} assets'.format(len(asset_list)))
+        if asset_list and type(asset_list)==list:
+            for asset_i in asset_list:
+                if GEE_PATHPREFIX + asset == asset_i:
+                    asset_found = True
+                    break
+        return asset_found
 
-    except:
-        logs.print_log("Asset list could not be retreived", "WARNING")
+    except Exception:
         return False
 
-    # Check if asset is in asset list
-    asset_found = False
-    for asset_x in asset_list:
-        if GEE_PATHPREFIX + asset == asset_x:
-            asset_found = True
+
+    
 
     logging.debug(f"Asset successfully found: {asset_found}")
     return asset_found
@@ -87,7 +98,12 @@ def check_folder_exists(path):
         asset = ee.data.getAsset(path)
         if asset:
             return asset['type'] in ['FOLDER','IMAGE_COLLECTION']
-    except:
-        print("Can't find folder")
+    except Exception as e:
+        logging.warning(e)
         return False
 
+def main():
+    pass
+
+if '__name__'=='__main__':
+    main()
