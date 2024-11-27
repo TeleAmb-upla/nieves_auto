7755fa1bd773dd2042dc7c64caef3ef6e61fade4
Erick G <9361923+silversky54@users.noreply.github.com>
2024-11-27 18:09:05 -0300

    updated utilities.dockers changed check_docker_secret to raise error ony once.

--- snow_ipa/utilities/dockers.py
+++ snow_ipa/utilities/dockers.py
@@ -1,6 +1,8 @@
 import pathlib
 import logging
 
+DOCKER_SECRET_LINUX_PATH = "/var/run/secrets"
+
 
 def check_docker_secret(var: str) -> str:
     """
@@ -13,18 +15,17 @@ def check_docker_secret(var: str) -> str:
     Returns:
         Returns the path to use to read var value or None if file doesn't exist
     """
-    docker_secrets_path = "/var/run/secrets"
-    # Search for file path provided
-    try:
-        var_path = pathlib.Path(var)
-        docker_var_path = pathlib.Path(docker_secrets_path, var)
-        if var_path.exists():
-            return var_path.as_posix()
 
-        # else, if only file name is provided, try to find the file in docker secrets
-        elif len(var_path.parts) == 1 and docker_var_path.exists():
+    # Search for file path provided
+
+    var_path = pathlib.Path(var)
+    if var_path.exists():
+        return var_path.as_posix()
+
+    # else, if only file name is provided, try to find the file in docker secrets
+    if len(var_path.parts) == 1:
+        docker_var_path = pathlib.Path(DOCKER_SECRET_LINUX_PATH, var)
+        if docker_var_path.exists():
             return docker_var_path.as_posix()
-        else:
-            raise FileNotFoundError(f"file not found: {var}")
-    except (FileNotFoundError, Exception):
-        raise
+
+    raise FileNotFoundError(f"file not found: {var}")
