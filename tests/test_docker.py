7e15c4abe49a4a6c0ae94000e7cdbceaa8c9d899
Erick G <9361923+silversky54@users.noreply.github.com>
2024-11-27 18:02:57 -0300

    added tests for utilities.docker

---
+++ tests/test_docker.py
@@ -0,0 +1,26 @@
+import pytest
+import pathlib
+from unittest import mock
+from snow_ipa.utilities.dockers import check_docker_secret, DOCKER_SECRET_LINUX_PATH
+
+
+class TestCheckDockerSecret:
+    @mock.patch("pathlib.Path.exists")
+    def test_file_exists_in_given_path(self, mock_exists):
+        mock_exists.side_effect = lambda: True
+        var = "/path/to/file"
+        assert check_docker_secret(var) == var
+
+    @mock.patch("pathlib.Path.exists")
+    def test_file_exists_in_docker_secrets_path(self, mock_exists):
+        mock_exists.side_effect = [False, True]
+        var = "file"
+        expected_path = pathlib.Path(DOCKER_SECRET_LINUX_PATH, var).as_posix()
+        assert check_docker_secret(var) == expected_path
+
+    @mock.patch("pathlib.Path.exists")
+    def test_file_not_found(self, mock_exists):
+        mock_exists.return_value = False
+        var = "nonexistent_file"
+        with pytest.raises(FileNotFoundError, match=f"file not found: {var}"):
+            check_docker_secret(var)
