ce5c837294e2ca4f1b04f77b297d0e0e249771d8
Erick G <9361923+silversky54@users.noreply.github.com>
2025-05-16 12:48:38 -0400

    build: general improvements to project setup
    Enable import of package models and sub-packages with 'import snow_ipa'. Other updates to pyproject.toml for semantic versioning.

--- src/snow_ipa/__init__.py
+++ src/snow_ipa/__init__.py
@@ -1,3 +1,10 @@
+__all__ = ["main", "core", "services", "utils"]
+
+from . import main
+from . import core
+from . import services
+from . import utils
+
 # read version from installed package
 # from importlib.metadata import version
 
